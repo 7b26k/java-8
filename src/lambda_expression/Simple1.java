417a20eb47af8d032c989073c8a71f6cd5dfe01b
bilal7b26k <belalaltamashkamal@gmail.com>
2020-04-22 03:34:54 +0530

    lambda expression simple program

---
+++ src/lambda_expression/Simple1.java
@@ -0,0 +1,11 @@
+package lambda_expression;
+
+public class Simple1 {
+
+	public static void main(String[] args) {
+		System.out.println("simple program without lambda expression");
+		
+		
+	}
+
+}
